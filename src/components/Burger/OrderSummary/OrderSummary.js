73b2f50b63e73619deee735a9c466e6fc284c673
Chandan <chandan7791@gmail.com>
2018-09-06 11:21:34 +0530

    Changed filename import of Aux.js to Auxi.js for windows compatibility

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Auxi'
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {

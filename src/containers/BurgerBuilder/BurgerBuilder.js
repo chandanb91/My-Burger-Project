c7670e4ea53c8e71addda33e5bb3c48ce56785d4
Chandan <chandan7791@gmail.com>
2018-09-07 14:59:46 +0530

    Sending a post request using firebase and axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -83,7 +84,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continued!');
+       // alert('You Continued!');
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer: {
+               name: 'Chandan Gowda',
+               address: {
+                   street: 'MG Road',
+                   zipCode: '560001',
+                   Country: 'India'
+               },
+               email: 'tony@gmail.com'
+           },
+           deliveryMethod: 'fastest'
+       }
+       axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
     }
 
     render () {
